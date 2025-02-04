fde5a8321443fb80b385251f656fafbe66a9164b
Alberto <albertojunior848423803@gmail.com>
2025-02-04 11:20:01 +0300

    first commit

---
+++ src/main/java/com/example/AuthandFinance/AuthandFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthandFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthandFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthandFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthandFinance/AuthandFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthandFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthandFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
