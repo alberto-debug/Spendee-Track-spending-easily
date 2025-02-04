7eaa665df4414028ff1e96f4cb651e4b1c20b79e
Alberto <albertojunior848423803@gmail.com>
2025-02-04 17:41:47 +0300

    changes

--- src/main/java/com/example/AuthFinance/domain/user/User.java
+++ src/main/java/com/example/AuthFinance/domain/user/User.java
@@ -14,8 +14,8 @@ import lombok.Setter;
 public class User {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
 
   @Column(nullable = false, unique = true)
   private String name;
@@ -30,11 +30,11 @@ public class User {
 
   }
 
-  public Long getId() {
+  public String getId() {
     return id;
   }
 
-  public void setId(Long id) {
+  public void setId(String id) {
     this.id = id;
   }
 
@@ -62,7 +62,7 @@ public class User {
     this.password = password;
   }
 
-  public User(Long id, String name, String email, String password) {
+  public User(String id, String name, String email, String password) {
     this.id = id;
     this.name = name;
     this.email = email;
