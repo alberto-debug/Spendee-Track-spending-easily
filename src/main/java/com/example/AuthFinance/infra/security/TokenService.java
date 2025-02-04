191e56684cdb77d29e354e6d82b2cc5c0fb5ac35
Alberto <albertojunior848423803@gmail.com>
2025-02-05 00:24:17 +0300

    Token

--- src/main/java/com/example/AuthFinance/infra/security/TokenService.java
+++ src/main/java/com/example/AuthFinance/infra/security/TokenService.java
@@ -1,25 +1,35 @@
 package com.example.AuthFinance.infra.security;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.AuthFinance.domain.user.User;
 import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
 public class TokenService {
-
     private String secret;
 
     public String generateToken(User user){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
-        } catch (JWTCreationException exception) {
-            throw new RuntimeException("Error while Authenticating");
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt()
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error while authenticating");
         }
+    }
 
-
-
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
