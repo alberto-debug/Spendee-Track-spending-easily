6d56c1960943165027d21a6bc3fe3ece55d1936f
Alberto <albertojunior848423803@gmail.com>
2025-02-04 11:32:10 +0300

    first commit

--- src/main/java/com/example/AuthandFinance/AuthandFinanceApplication.java
+++ src/main/java/com/example/AuthandFinance/AuthandFinanceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthandFinanceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AuthandFinanceApplication.class, args);
 	}
 
