6e590893d3aba83a1a2f9933d92b85d40ee14edf
Alberto <albertojunior848423803@gmail.com>
2025-02-04 17:30:35 +0300

    chnages

--- src/main/java/com/example/AuthFinance/domain/user/User.java
+++ src/main/java/com/example/AuthFinance/domain/user/User.java
@@ -26,4 +26,46 @@ public class User {
   @Column(nullable = false)
   private String password;
 
+  public User(){
+    
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public User(Long id, String name, String email, String password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
 }
