506efe892ecb049fe1d8672c283a909aad0834c1
Alberto <albertojunior848423803@gmail.com>
2025-02-06 00:11:17 +0300

    Token

--- src/main/java/com/example/AuthFinance/infra/security/SecurityFilter.java
+++ src/main/java/com/example/AuthFinance/infra/security/SecurityFilter.java
@@ -1,11 +1,11 @@
 package com.example.AuthFinance.infra.security;
 
+import com.auth0.jwt.interfaces.Header;
 import com.example.AuthFinance.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,6 +24,16 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var
+
+        var token = this.recoverToken(request);
+        var login = tokenService.validateToken(token);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null){
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
     }
 }
