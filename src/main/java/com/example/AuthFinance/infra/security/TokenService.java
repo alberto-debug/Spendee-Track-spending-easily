ee60244a9806ff02c9eb614bbd07ea0d56683842
Alberto <albertojunior848423803@gmail.com>
2025-02-05 22:08:00 +0300

    Token

--- src/main/java/com/example/AuthFinance/infra/security/TokenService.java
+++ src/main/java/com/example/AuthFinance/infra/security/TokenService.java
@@ -10,14 +10,20 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.concurrent.ExecutionException;
 
 
 @Service
 public class TokenService {
     private String Token;
 
-    public void generateToken(User user){
+    public String generateToken(User user){
 
+        try {
+
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error while Authenticating");
+        }
     }
     private Instant generateExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
