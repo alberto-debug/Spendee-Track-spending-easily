cb4af3fb476c7fc7f224418cedf20cbe6f32dad7
Alberto <albertojunior848423803@gmail.com>
2025-02-06 00:00:51 +0300

    Token

--- src/main/java/com/example/AuthFinance/infra/security/SecurityFilter.java
+++ src/main/java/com/example/AuthFinance/infra/security/SecurityFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,6 +24,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+        var
     }
 }
