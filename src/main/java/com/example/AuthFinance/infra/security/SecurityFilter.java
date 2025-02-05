f97aa946777737962ceea8208ceb7dfd715ded3f
Alberto <albertojunior848423803@gmail.com>
2025-02-05 23:31:31 +0300

    Token

--- src/main/java/com/example/AuthFinance/infra/security/SecurityFilter.java
+++ src/main/java/com/example/AuthFinance/infra/security/SecurityFilter.java
@@ -1,9 +1,16 @@
 package com.example.AuthFinance.infra.security;
 
+import com.example.AuthFinance.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityFilter {
 
+    @Autowired
+    private TokenService tokenService;
 
+    @Autowired
+    private UserRepository userRepository;
+    
 }
