71b172044b951d9e88d3c459d104f0c094987b68
Alberto <albertojunior848423803@gmail.com>
2025-02-06 15:02:35 +0300

    CustomUserDetails final implementation

--- src/main/java/com/example/AuthFinance/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/AuthFinance/infra/security/CustomUserDetailsService.java
@@ -11,6 +11,7 @@ import com.example.AuthFinance.repository.UserRepository;
 
 import java.util.ArrayList;
 
+//load the user details to spring security to validate it during authentication
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -20,5 +21,6 @@ public class CustomUserDetailsService implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
   }
 }
