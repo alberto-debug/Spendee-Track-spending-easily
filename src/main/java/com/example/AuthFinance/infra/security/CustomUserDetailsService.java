5d52fd6b70281f236ba6e8e62ecb14b1d9b0a70b
Alberto <albertojunior848423803@gmail.com>
2025-02-06 13:53:50 +0300

    cudtom

--- src/main/java/com/example/AuthFinance/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/AuthFinance/infra/security/CustomUserDetailsService.java
@@ -1,4 +1,13 @@
 package com.example.AuthFinance.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.AuthFinance.repository.UserRepository;
+
+@Service
 public class CustomUserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
 }
