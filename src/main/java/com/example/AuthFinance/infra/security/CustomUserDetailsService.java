0920a305ac2107804465c59355adfdea9f208217
Alberto <albertojunior848423803@gmail.com>
2025-02-06 14:57:52 +0300

    changes

--- src/main/java/com/example/AuthFinance/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/AuthFinance/infra/security/CustomUserDetailsService.java
@@ -6,9 +6,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.example.AuthFinance.repository.UserRepository;
+
+import java.util.ArrayList;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
@@ -19,6 +20,5 @@ public class CustomUserDetailsService implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
-    return null;
   }
 }
