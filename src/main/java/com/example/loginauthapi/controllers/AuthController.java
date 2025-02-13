dcabddbc7749851d000df12273dd78b5171a9e9e
Alberto <albertojunior848423803@gmail.com>
2025-02-13 21:24:06 +0300

    changes

--- src/main/java/com/example/loginauthapi/controllers/AuthController.java
+++ src/main/java/com/example/loginauthapi/controllers/AuthController.java
@@ -1,12 +1,10 @@
 package com.example.loginauthapi.controllers;
 
-import com.example.loginauthapi.domain.role.AppRole;
 import com.example.loginauthapi.domain.user.User;
 import com.example.loginauthapi.dto.LoginRequestDTO;
 import com.example.loginauthapi.dto.RegisterRequestDTO;
 import com.example.loginauthapi.dto.ResponseDTO;
 import com.example.loginauthapi.infra.security.TokenService;
-import com.example.loginauthapi.repositories.RoleRepository;
 import com.example.loginauthapi.repositories.UserRepository;
 import com.example.loginauthapi.validation.ValidationException;
 import com.example.loginauthapi.validation.ValidationService;
@@ -16,11 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import javax.management.relation.Role;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
 
 @Data
 @RestController
