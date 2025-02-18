de09a860ed1b8b776a77b4ccb2cef311e17df2e5
Alberto <albertojunior848423803@gmail.com>
2025-02-18 16:41:33 +0300

    changes

--- src/main/java/com/example/loginauthapi/services/TransactionService.java
+++ src/main/java/com/example/loginauthapi/services/TransactionService.java
@@ -1,5 +1,7 @@
 package com.example.loginauthapi.services;
 
+import com.example.loginauthapi.domain.user.Transaction;
+import com.example.loginauthapi.domain.user.User;
 import com.example.loginauthapi.repositories.TransactionRepository;
 import com.example.loginauthapi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,5 +14,11 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final UserRepository userRepository;
 
+    public Transaction addTransaction(Transaction transaction, String userEmail){
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(()-> new RuntimeException("User Not Found"));
 
+        transaction.
+
+    }
 }
