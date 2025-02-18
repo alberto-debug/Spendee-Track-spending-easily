798149e541c5b5c51c09f33653fa8302e240beb4
Alberto <albertojunior848423803@gmail.com>
2025-02-18 16:38:23 +0300

    changes

--- src/main/java/com/example/loginauthapi/services/TransactionService.java
+++ src/main/java/com/example/loginauthapi/services/TransactionService.java
@@ -1,4 +1,16 @@
 package com.example.loginauthapi.services;
 
+import com.example.loginauthapi.repositories.TransactionRepository;
+import com.example.loginauthapi.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
+
+
 }
